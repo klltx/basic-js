4c22d99705669fd09f1d08a15ef95d644bb820e8
klltx <kostya.svetashov.01@gmail.com>
2021-02-28 21:31:34 +0400

    feat: add solution count-cats.js and what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if(date == null) {
+    return 'Unable to determine the time of year!';
+  } else if(date instanceof Date && !isNaN(date)){
+    let currentMonth = date.getMonth() + 1;
+    if(currentMonth > 2 && currentMonth < 6) return 'spring';
+    else if(currentMonth > 5 && currentMonth < 9) return 'summer';
+    else if(currentMonth > 8 && currentMonth < 12) return 'autumn';
+    else return 'winter';
+  } else {
+    throw new RangeError('');
+  }
 };
