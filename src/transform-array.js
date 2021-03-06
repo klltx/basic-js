9d62008be713ca667302eef6d5ae051a9ff81dc7
klltx <kostya.svetashov.01@gmail.com>
2021-03-06 10:49:44 +0400

    start: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)) throw new RangeError('');
+
+  let res = [];
+
+  for(let i = 0; i < arr.length; i++){
+    switch(arr[i]){
+      case '--discard-next':
+        i = i + 2;
+        break;
+      case '--discard-prev':
+        res.pop();
+        break;
+      case '--double-next':
+        if(i + 1 < arr.length)
+          res.push(arr[i + 1]);
+        break;
+      case '--double-prev':
+        if(i - 1 >= 0)
+          res.push(arr[i - 1]);
+        break;
+      default:
+        res.push(arr[i]);
+        break;
+    }
+  }
+
+  return res;
 };
